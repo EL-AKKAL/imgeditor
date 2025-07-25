0a3c3ba25f473c38ff7d164aeff1d14b9cc8c7cb
EL AKKAL AYOUB <ayoub.elakkal@disklosure.net>
2025-07-25 14:59:20 +0100

    fixing mobile layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import Toolbar from '@/components/Toolbar.vue'
-import EditorCanvas from '@/components/EditorCanvas.vue'
-import FiltersPanel from '@/components/FiltersPanel.vue'
+import Toolbar from "@/components/Toolbar.vue";
+import EditorCanvas from "@/components/EditorCanvas.vue";
+import FiltersPanel from "@/components/FiltersPanel.vue";
 // @ts-ignore
-import StickersPanel from '@/components/StickersPanel.vue'
+import StickersPanel from "@/components/StickersPanel.vue";
 </script>
 
 <template>
   <main>
     <div class="bg-white rounded-lg shadow-sm !px-8 !py-5">
       <h2 class="text-2xl !mb-5">My Images Editor</h2>
-      <div class="grid grid-cols-2 gap-5 xs:grid-cols-1">
+      <div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
         <FiltersPanel />
         <EditorCanvas />
       </div>
--- src/components/EditorCanvas.vue
+++ src/components/EditorCanvas.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="!box-border w-full min-w-80">
-    <canvas v-show="store.editedImage" ref="canvas" class="w-full h-full"></canvas>
-    <PlaceHolder v-show="!store.editedImage" />
+  <div class="!box-border w-full min-w-0 sm:min-w-80">
+    <canvas
+      v-show="store.editedImage"
+      ref="canvas"
+      class="w-full h-full"
+    ></canvas>
+    <PlaceHolder class="w-full h-full" v-show="!store.editedImage" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
-import { useImageEditorStore } from '@/stores/editor'
-import PlaceHolder from '@/components/PlaceHolder.vue'
+import { onMounted, ref, watch } from "vue";
+import { useImageEditorStore } from "@/stores/editor";
+import PlaceHolder from "@/components/PlaceHolder.vue";
 
-const imagePlaceholder = new URL('/image-placeholder.png', import.meta.url).href
-const store = useImageEditorStore()
-const canvas = ref<HTMLCanvasElement>()
-const ctx = ref<CanvasRenderingContext2D | null>(null)
+const imagePlaceholder = new URL("/image-placeholder.png", import.meta.url)
+  .href;
+const store = useImageEditorStore();
+const canvas = ref<HTMLCanvasElement>();
+const ctx = ref<CanvasRenderingContext2D | null>(null);
 
 onMounted(() => {
-  if (canvas.value) ctx.value = canvas.value.getContext('2d')
-  redraw()
-})
+  if (canvas.value) ctx.value = canvas.value.getContext("2d");
+  redraw();
+});
 
 watch(
   () => [
@@ -35,10 +40,10 @@ watch(
     store.editedImage,
   ],
   redraw,
-  { deep: true },
-)
+  { deep: true }
+);
 
 function redraw() {
-  if (canvas.value && ctx.value) store.redrawCanvas(canvas.value, ctx.value)
+  if (canvas.value && ctx.value) store.redrawCanvas(canvas.value, ctx.value);
 }
 </script>
